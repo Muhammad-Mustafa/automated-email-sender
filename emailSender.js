d34ad1d4d0f9b12904d4d55838194a21ab775f8e
Muhammad-Mustafa <mustafaraziq30@gmail.com>
2020-07-22 18:07:53 +0500

    Email.js Implemented

--- emailSender.js
+++ emailSender.js
@@ -1,23 +1,19 @@
 import React from "react";
 import * as emailjs from "emailjs-com";
 export default function emailSender() {
-  function sendEmail(e) {
-    emailjs
-      .sendForm(
-        "default_service",
-        "decentralized_voting_platform",
-        e.target,
-        "lb9GeK9CH5WP2u1U7wCuO"
-      )
-      .then(
-        (result) => {
-          console.log(result.text);
-        },
-        (error) => {
-          console.log(error.text);
-        }
-      );
+  var params = {
+    to_email: email, //the email you want to send the mail
+                     //On email.js where the require  the to_email you just write {{to_email}} 
+                     //if you are still confuse just check helper.jpg
+                     //You can add more params that you want 
   }
+    var service_id = "default_service";
+    var user_id = "Your User Id Here";
+
+    // var service_id = "default_service";
+    var template_id = "Your templete id here"; 
+    emailjs.send(service_id, template_id, params, user_id)
+  
   return (
     <div>
       <form className="contact-form" /*onSubmit={sendEmail}*/>
